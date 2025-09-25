027ec702d9d180088c2c1e338f6bf519f455c9d0
Maria28420 <Lena.os01012008@gmail.com>
2025-09-25 08:43:36 -0300

    Inserir tarefa

--- tarefa_inserir.php
+++ tarefa_inserir.php
@@ -9,6 +9,32 @@
     ?>
 </head>
 <body>
-    
+    <?php
+     //1º PASSO: Capturar os dados de entrada
+     $descricao = $_POST["txtDescricao"];
+     $data_entrega = $_POST["txtData"];
+     $prioridade = $_POST["txtPrioridade"];
+     $responsavel = $_POST["txtResponsavel"];
+
+     //2º PASSO: Montar o comando SQL-insert
+    $sql= "INSERT INTO tarefa(descricao,data_entrega,prioridade,responsavel) VALUES(?,?,?,?)";
+
+    //3º PASSO: Preparar o comando SQL para ser executa
+    //Ou seja,relacionar o comando SQL que será executado
+    $comando=$conexao->prepare($sql);
+
+    //4º PASSO: Vincular os ??? com as variáveis de entrada de dados
+    $comando->bind_param("ssss",$descricao,$data_entrega,$prioridade,$responsavel);
+
+    //5º PASSO: Executar o comando 
+    if ($comando->execute())
+    {
+        echo "<h1> Tarefa agendada</h1>";
+    }
+    else
+    {
+        echo "<h1>Erro!</h1>";
+    }
+    ?>
 </body>
 </html>
