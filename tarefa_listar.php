19ce7f4170c1ec17ca9c447d9b07cb5fa8e61556
Maria28420 <Lena.os01012008@gmail.com>
2025-10-02 08:53:40 -0300

    Lista tarefa

--- tarefa_listar.php
+++ tarefa_listar.php
@@ -30,7 +30,31 @@
                                 <td>Prioridade</td>
                                 <td>Responsável</td>
                             </tr>
+                          <?php
+                            //1º Passo: Montar o comando SQL que será executado
+                            $sql="SELECT * from tarefa Order by data_entrega desc";
+                            //2º Passo: Preparar o comando para ser executado na conexão de dados
+                            $comando= $conexao-> prepare($sql);
+                            //3º Passo: Executar o comando
+                            $comando-> execute();
+                            //4º Passo: capturar os resultados trazidos pelo mysql
+                            $resultado= $comando->get_result();
+                            //5º Passo: Pegar todas as linhas de dados que estão em Resultados
+                            while($registro = $resultado->fetch_assoc())
+                                {
+                                $descricao = $registro["descricao"];
+                                $data_entrega = $registro["data_entrega"];
+                                $prioridade = $registro["prioridade"];
+                                $responsavel = $registro["responsavel"];
 
+                          ?>
+                          <tr>
+                            <td> <?php echo $descricao ?> </td>
+                            <td> <?php echo $data_entrega ?> </td>
+                            <td> <?php echo $prioridade?> </td>
+                            <td> <?php echo $responsavel?> </td>
+                          </tr>
+                          <?php } ?>
 
                         </table>
                     </div>
